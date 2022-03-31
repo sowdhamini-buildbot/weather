77be084e4cbe6e93c76fbe067f3610bb5ed1574d
chodavarapu sowdhamini <94843433+sowdhamini-buildbot@users.noreply.github.com>
2022-03-31 11:10:46 +0530

    manipulated data updated

--- api.js
+++ api.js
@@ -1,23 +1,27 @@
 // api key : 82565a74ce3e39072e71ac0895d785d9
+function getWeathername(cityName) {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=82565a74ce3e39072e71ac0895d785d9&units=metric`;
+  axios
+    .get(url)
+    .then(function (response) {
+      // handle success
+      document.getElementById(
+        "message"
+      ).innerText = `it is now ${response.data.main.temp}°C`;
+    })
+    .catch(function (error) {
+      // document.getElementById("message").value;
+      alert("please enter the valid city name");
+    });
+}
 
-const url =
-  "http://api.openweathermap.org/data/2.5/weather?q=Visakhapatnam&appid=82565a74ce3e39072e71ac0895d785d9";
-// Make a request for a user with a given ID
-axios
-  .get(
-    "http://api.openweathermap.org/data/2.5/weather?q=Visakhapatnam&appid=82565a74ce3e39072e71ac0895d785d9"
-  )
-  .then(function (response) {
-    // handle success
-    document.getElementById(
-      "message"
-    ).innerText = `it is now ${response.data.main.temp}C`;
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    console.log("all done");
-    // always executed
-  });
+window.onload = function () {
+  document.getElementById("send").onclick = function () {
+    const cityName = document.getElementById("city").value;
+    if (document.getElementById("city").value.trim() !== "") {
+      console.log("value", document.getElementById("city").value);
+      getWeathername(cityName);
+    }
+  };
+};
+
